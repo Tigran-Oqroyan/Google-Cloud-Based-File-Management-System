001db56c569931b6e0d224e9f7229cba379414bb
TigranBoomTech <tigrano@boomte.ch>
2024-12-13 11:07:07 +0400

    global styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+// eslint-disable-next-line no-unused-vars
+import styles from './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
